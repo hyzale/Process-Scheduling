cd65e31bd3a232d2a09aff86d39ae37310874aa4
Alexa Huang <hyzale@berkeley.edu>
2018-10-16 22:31:39 -0400

    make arraylist as a subclass of list

--- ArrayList.hpp
+++ ArrayList.hpp
@@ -1,61 +1,61 @@
-#ifndef ARRAY_LIST
-#define ARRAY_LIST
+#ifndef ARRAYLIST
+#define ARRAYLIST
+
+#include "List.hpp"
 
 template <class item_t>
-class ArrayList
+class ArrayList : public List<item_t>
 {
-  private:
-
+  protected:
+   item_t* items;
+   int size;
+   int capacity;
 
    //Resizes the ArrayList to the target capacity.
    void resize(int cap);
    void resizeIfFull();
 
   public:
-
-   item_t* items;
-   int size;
-   int capacity;
    //Creates an empty list with capacity 10
    ArrayList();
    //Creates a list with the given capacity
    ArrayList(int capacity);
-   ~ArrayList();
+   virtual ~ArrayList();
 
    //Pushes item to the back of the list
-   void pushBack(const item_t& item);   
+   virtual void pushBack(const item_t& item);   
    //Removes, but does not return, the last item
-   void popBack();
+   virtual void popBack();
    //Returns a reference to the last item (does not remove it).
-   const item_t& getBack() const;
+   virtual const item_t& getBack() const;
    
    //Pushes item to the front of the list
-   void pushFront(const item_t& item);
+   virtual void pushFront(const item_t& item);
    //Removes, but does not return, the first item
-   void popFront();
+   virtual void popFront();
    //Returns a reference to the first item (does not remove it).
-   const item_t& getFront() const;
+   virtual const item_t& getFront() const;
 
    //Returns a reference to the item at the given index
-   const item_t& getItem(int index) const;
+   virtual const item_t& getItem(int index) const;
    //Sets the item at the given index
-   void setItem(int index, const item_t& item);
+   virtual void setItem(int index, const item_t& item);
 
    //Inserts the given item at the given index
    //(items at index or beyond shift up one position)
    //Inserting just past the last element (i.e. position
    //size) shoud be equivalent to pushBack.
-   void insert(int index, const item_t& item);
+   virtual void insert(int index, const item_t& item);
    //Removes the item at the given index
    //(items beyond index shift down one position)
-   void remove(int index);
+   virtual void remove(int index);
 
    //Returns the number of items in the list
-   int getSize() const;
+   virtual int getSize() const;
    //Returns true if the list is empty (false otherwise)
-   bool isEmpty() const;
+   virtual bool isEmpty() const;
    //Returns the size of the array that contains the list
-   int getCapacity() const;
+   virtual int getCapacity() const;
 };
 
 #include "ArrayList.tpp"
