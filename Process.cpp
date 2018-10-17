289eed9504be1e512a637dd6783f0c6c050437d2
Alexa Huang <hyzale@berkeley.edu>
2018-10-16 23:00:12 -0400

    stupid comment for process.cpp

--- Process.cpp
+++ Process.cpp
@@ -4,22 +4,24 @@
 
 using namespace std;
 
+//Constructor with process id and set cpuTime and waitTime to 0
 Process::Process(int id) : id(id)
 {
    cpuTime = 0;
    waitTime = 0;
 }
 
+//return the ID of process.
 int Process::getID() const
 {
    return id;
 }
-
+//return the cpu time process used.
 int Process::getCPUTime() const
 {
    return cpuTime;
 }
-
+//return the wait time (ready to run but not).
 int Process::getWaitTime(int curCycleNum) const
 {
    int additionalWait = curCycleNum - lastTime;
@@ -30,16 +32,19 @@ int Process::getWaitTime(int curCycleNum) const
    return waitTime + additionalWait;
 }
 
+//constructor of the CPU bound process with the last time process set as 0
 CPUBoundProcess::CPUBoundProcess(int id) : Process(id)
 {
    lastTime = 0;
 }
 
+//return false as process is never blocked
 bool CPUBoundProcess::isBlocked(int curCycleNum) const
 {
    return false;
 }
 
+//return sliceSize that process uses all the cpu time
 int CPUBoundProcess::useCPU(int curCycleNum, int sliceSize)
 {
    //The process uses all CPU time given to it
@@ -53,6 +58,7 @@ int CPUBoundProcess::useCPU(int curCycleNum, int sliceSize)
    return sliceSize;
 }
 
+//constructor, set with range of cpu burst time, i/o request time
 IOBoundProcess::IOBoundProcess(int id) : Process(id), minBurst(5), maxBurst(15), minIO(50), maxIO(200)
 {
    lastTime = 0;
@@ -60,11 +66,19 @@ IOBoundProcess::IOBoundProcess(int id) : Process(id), minBurst(5), maxBurst(15),
    remainingBurst = 0;
 }
 
+//the process maybe blocked
+//process blocked if the cycle number is smaller than 
+//the time when the process will unblock, vice versa.
 bool IOBoundProcess::isBlocked(int curCycleNum) const
 {
    return curCycleNum < blockedUntil;
 }
 
+
+
+//The time using cpu...
+//btw feel like the provided comment explain this very clearly.
+//not sure why we need to comment again.
 int IOBoundProcess::useCPU(int curCycleNum, int sliceSize)
 {
    //If the process was blocked, and is now unblocked,
